d36756ed359b953668e6eb19b63b3b98b604a57b
Milan Djurdjevic <milan_djurdjevic@icloud.com>
2023-11-17 00:51:29 +0100

    Add more tests

--- src/Audentity.Tests/AuditTests.cs
+++ src/Audentity.Tests/AuditTests.cs
@@ -13,6 +13,14 @@ public class AuditTests
     }
 
     [Fact]
+    public Task RangeAdded_Collects()
+    {
+        IEnumerable<Model> entities = Enumerable.Range(0, 5).Select(_ => new Model());
+        _database.AddRange(entities);
+        return Verify(_database.ChangeTracker.Audit());
+    }
+
+    [Fact]
     public Task Modified_Collects()
     {
         Model entity = new();
@@ -25,12 +33,39 @@ public class AuditTests
     }
 
     [Fact]
+    public Task RangeModified_Collects()
+    {
+        IEnumerable<Model> entities = Enumerable.Range(0, 5).Select(_ => new Model()).ToArray();
+        _database.AddRange(entities);
+        _database.SaveChanges();
+
+        foreach (Model entity in entities)
+        {
+            entity.Property = Guid.NewGuid().ToString();
+            entity.Owned.Value = Guid.NewGuid().ToString();
+            entity.Owned.Immutable = entity.Owned.Immutable with { Value = Guid.NewGuid().ToString() };
+        }
+
+        return Verify(_database.ChangeTracker.Audit());
+    }
+
+    [Fact]
     public Task Deleted_Collects()
     {
         Model entity = new();
         _database.Add(entity);
         _database.SaveChanges();
         _database.Remove(entity);
+        return Verify(_database.ChangeTracker.Audit());
+    }
+
+    [Fact]
+    public Task RangeDeleted_Collects()
+    {
+        IEnumerable<Model> entities = Enumerable.Range(0, 5).Select(_ => new Model()).ToArray();
+        _database.AddRange(entities);
+        _database.SaveChanges();
+        _database.RemoveRange(entities);
         return Verify(_database.ChangeTracker.Audit());
     }
 }
